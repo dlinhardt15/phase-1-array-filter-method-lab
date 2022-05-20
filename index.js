714e72234bc6a1a577af7a0a63e5ab60d6d5935d
Danny Linhardt <daniel.linhardt@gmail.com>
2022-05-20 12:20:32 -0400

    commit tree f7a30e76b8a1f26c5e79af41dd92b98eb9610942

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching (drivers, driver) {
+    return drivers.filter(function(member) {
+        return member === driver || member == driver.toLowerCase();
+    })
+}
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(function(driver) {
+        return driver.startsWith(letters)
+    })
+};
+
+function matchName (drivers, string) {
+    return drivers.filter(function (driver) {
+        return driver.name.includes(string)
+    })
+}
